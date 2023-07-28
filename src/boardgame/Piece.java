26f502ec5402ab699c7004c16b67af0baabfee00
hochiminh1996 <ambrsp@hotmail.com>
2023-07-27 21:50:09 -0300

    Métodos abstratos e não abstratos para validação de movimentos possíveis - p1

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,9 +1,9 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	// classe peça. Voltanda para as especificidades de peça
 
-	protected Position position;//acessível apenas dentro do pacote e subclasses
+	protected Position position;// acessível apenas dentro do pacote e subclasses
 	private Board board; // é nossa associação (composição) com a classe tabuleiro
 
 	public Piece() {
@@ -17,7 +17,30 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-	
 
+	// método abstrato que precisa de implementação nas classes especifica.
+	// Nesse caso, cada sub-classe (peças -> torre, rei, rainha) tem sua regra
+	// especifica de movimentação. Esse método valida todas as movimentações
+	// possíveis
+	public abstract boolean[][] possibleMoves();
+
+	// Verifica se uma peça pode se mover para uma determinada posição
+	public boolean possibileMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+		// está chamando o método abstrato, que cada classe filha implementa.
+		// Afinal, cada peça tem sua regra de movimentação. Esse método apenas está
+		// verificando
+		// se o movimento solicitado é plausível dentro da regra da peça
+
+	}
+
+	// verifica se há pelo PELO MENOS UMA possição na matriz para realizar a
+	// movimentação. Esse movimento é importante para validar se a peça está
+	// encurralada e não pode se movimentar mais
+	//
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();// novamente chamando o nosso método que será implementado
+		// e cada peça
+	}
 
 }
