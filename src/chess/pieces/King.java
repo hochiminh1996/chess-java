2dcfc359e57a840783ee072292213797c2f0efef
hochiminh1996 <ambrsp@hotmail.com>
2023-07-31 19:44:11 -0300

    Implementando os movimentos possíveis do rei: cima, baixo, nordeste,noroeste,sudoeste e sudeste

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,15 @@ public class King extends ChessPiece {
 		return "K";
 	}
 
+	// vai validar se o rei pode movimentar para uma posição
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		
+		// pode se mover para tal posição se a posição for diferente de null e for uma
+		// peça inimiga
+		return p == null || p.getColor() != getColor();
+	}
+
 	// Polimorfismo de sobreposição (mesma assinatura, implementação diferente)
 
 	// implementando o método abstrato da classe mãe piece. Embora a classe mãe mais
@@ -26,10 +36,93 @@ public class King extends ChessPiece {
 	// implementar
 	@Override
 	public boolean[][] possibleMoves() {
-		
+
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		// Cria uma matriz, do tipo boolean, com as dimensões definidas no tabuleiro :
 		// linha/coluna. Por padrão, todas as posições da matriz começam com false.
+
+		Position p = new Position(0, 0);
+
+		// testando a posição acima
+		p.setValues(position.getRow() - 1, position.getColumn());
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			// se a peça do tabuleiro existir e for possível mover, move.
+
+			mat[p.getRow()][p.getColumn()] = true;
+			// seta true na posição que é possível mover
+		}
+
+		// verificando posição abaixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			// se a peça do tabuleiro existir e for possível mover, move.
+
+			mat[p.getRow()][p.getColumn()] = true;
+			// seta true na posição que é possível mover
+		}
+
+		// verificando a posição à esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			// se a peça do tabuleiro existir e for possível mover, move.
+
+			mat[p.getRow()][p.getColumn()] = true;
+			// seta true na posição que é possível mover
+		}
+
+		// verificando a posição à direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			// se a peça do tabuleiro existir e for possível mover, move.
+
+			mat[p.getRow()][p.getColumn()] = true;
+			// seta true na posição que é possível mover
+		}
+
+		// verificando a posição noroeste do rei
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			// se a peça do tabuleiro existir e for possível mover, move.
+
+			mat[p.getRow()][p.getColumn()] = true;
+			// seta true na posição que é possível mover
+		}
+
+		// verificando a posição nordeste do rei
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			// se a peça do tabuleiro existir e for possível mover, move.
+
+			mat[p.getRow()][p.getColumn()] = true;
+			// seta true na posição que é possível mover
+		}
+
+		// verificando a posição sudoeste do rei
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			// se a peça do tabuleiro existir e for possível mover, move.
+
+			mat[p.getRow()][p.getColumn()] = true;
+			// seta true na posição que é possível mover
+		}
+
+		// verificando a posição sudeste do rei
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			// se a peça do tabuleiro existir e for possível mover, move.
+
+			mat[p.getRow()][p.getColumn()] = true;
+			// seta true na posição que é possível mover
+		}
+
 		return mat;
 	}
 
